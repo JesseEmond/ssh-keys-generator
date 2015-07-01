dc793b04a6f7bcb905418e0049b800347fe4f8d2
Jesse Emond <emond.jesse@gmail.com>
2015-06-30 21:30:46 -0400

    + primes test file with 1024 random prime test (fails)

---
+++ lib/test_primes.py
@@ -0,0 +1,13 @@
+import unittest
+import primes
+import random, math
+
+class TestPrimes(unittest.TestCase):
+    def test_random_prime_1024(self):
+        random.seed(42)
+
+        for i in range(1000):
+            prime = primes.random_prime(1024)
+            self.assertEqual(1024, math.ceil(math.log(prime, 2)))
+            self.assertTrue(primes.is_prime_rabin_miller(prime))
+
