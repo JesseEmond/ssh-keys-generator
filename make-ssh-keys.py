f0d8af7ac6c16808fc075f7f62ac21b95a6b92ae
Jesse Emond <emond.jesse@gmail.com>
2015-06-30 20:59:41 -0400

    * default not verbose

--- make-ssh-keys.py
+++ make-ssh-keys.py
@@ -7,7 +7,7 @@ parser.add_argument('--out', '-o', metavar='output-file', default='id_rsa',
                     help='File prefix for the key generated (output-file and output-file.pub)')
 parser.add_argument('--bits', '-b', default=4096, type=int,
                     help='Length (in bits) of the key generated')
-parser.add_argument('--verbose', '-v', default=True, type=bool,
+parser.add_argument('--verbose', '-v', default=False, type=bool,
                     help='If the current state should be print as the keys are generated.')
 
 args = parser.parse_args()
